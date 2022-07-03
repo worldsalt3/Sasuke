6123a339ff597d052423face9971d81417eded87
Worldsalt3 <riliwanhazzan@gmail.com>
2022-07-03 10:06:44 +0100

    designed loginpage desktop view

---
+++ src/Components/Header/Header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Header.style.scss'
+
+
+const Header = () => (
+    <header>
+
+    </header>
+)
+
+export default Header
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import {ReactComponent as Logo} from '../assets/Logo.svg'
+import {ReactComponent as PabloSignIn} from '../assets/pablo-sign-in.svg'
+
+import './Login.style.scss'
+
+const Login = () => (
+  <main className='login-container'>
+    <section className='img-section'>
+      <Logo />
+      <PabloSignIn className='illustration' />
+    </section>
+    <section className='login-section'>
+      <h1>Welcome!</h1>
+      <p>Enter details to login.</p>
+      <form action='' className='form-container'>
+        <input type='email' placeholder='Email' className='input' />
+        <input type='password' placeholder='Password' className='input' />
+        <p>FORGET PASSWORD?</p>
+        <input type='submit' className='submit-input' value='LOG IN' />
+      </form>
+    </section>
+  </main>
+)
+
+export default Login
